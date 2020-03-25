b7d87aace04900121f755892e7e11b73a0de49a3
Alex Withers <alexwithers2@gmail.com>
2020-03-25 11:42:36 -0500

    adding changes since january

--- run.sh
+++ run.sh
@@ -14,18 +14,17 @@ python3 code/points_by_time.py $1
 
 ./Global_Planner around_pit
 
-python3 Python_Viz/display_global_plan.py
+python3 Python_Viz/display_global_plan.py    #Uncomment this before running on PC
 
 # python3 code/visualize.py
 
 # Move to correct folder
 
-str="pattern"$1
+#str="pattern"$1
 
-mkdir -p $str; mv data/waypoints.csv  data/global_plan.png data/lander_to_pit_trajectory.csv data/path_location_mapping.csv $str
-
-str="pattern"$1 
+#mkdir -p $str; mv data/waypoints.csv  data/global_plan.png data/lander_to_pit_trajectory.csv data/path_location_mapping.csv $str
+ 
 #Pit edges are generated externally and need to be copied manually if it is changed.
-mv $str/waypoints.csv $str/global_waypoints.csv
-cp $str"/global_waypoints.csv" $str"/pit_edges.csv" $str"/global_plan.png" /home/hash/catkin_ws/src/visualization/data
-cp $str"/global_waypoints.csv" $str"/path_location_mapping.csv"  $str"/lander_to_pit_trajectory.csv" /home/hash/catkin_ws/src
+#mv $str/waypoints.csv $str/global_waypoints.csv
+#cp $str"/global_waypoints.csv" $str"/pit_edges.csv" $str"/global_plan.png" /home/hash/catkin_ws/src/visualization/data
+#cp $str"/global_waypoints.csv" $str"/path_location_mapping.csv"  $str"/lander_to_pit_trajectory.csv" /home/hash/catkin_ws/src
